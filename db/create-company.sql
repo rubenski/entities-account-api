3eef678cf9510769c74fa70898a874a8a59ba816
Ruben van Loen <ruben.van.loen@ing.nl>
2018-05-14 08:05:17 +0200

    Make commerce number unique

--- db/create-company.sql
+++ db/create-company.sql
@@ -4,7 +4,7 @@ CREATE TABLE f_company (
   street varchar(80) NOT NULL,
   number varchar(80) NOT NULL,
   zip varchar(10) NOT NULL,
-  commerceNumber varchar(20) NOT NULL,
+  commerceNumber varchar(20) NOT NULL UNIQUE,
   country varchar(3) NOT NULL,
   website varchar(30)
 )
