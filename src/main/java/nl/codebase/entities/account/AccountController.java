d94311ec36ff448fa870530947277126e5a1eb3e
Ruben van Loen <rubenski@gmail.com>
2018-05-21 18:42:46 +0200

    Added warning

--- src/main/java/nl/codebase/entities/account/AccountController.java
+++ src/main/java/nl/codebase/entities/account/AccountController.java
@@ -2,7 +2,6 @@ package nl.codebase.entities.account;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
